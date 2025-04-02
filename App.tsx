496a8c75a195a20ca2795a8187cb2195c9c44158
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-02 23:36:55 +0200

    refactor: remove unused stack navigator import from App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,5 @@
 import "./global.css";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StackNavigator } from "./src/navigation/StackNavigator";
-
-const Stack = createNativeStackNavigator();
 
 export const App = () => {
   return <StackNavigator />;
