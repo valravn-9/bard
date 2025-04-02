3e860160b1e6a9c22e8044ca50802c5e2509dd1b
Daniel Brandl <daniel.brandl@haufe-akademie.de>
2025-04-02 21:41:19 +0200

    chore: add initial configuration for Tailwind CSS and NativeWind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./**/*.{js,jsx,ts,tsx}", "!./node_modules/**/*"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  darkMode: "class",
+};
